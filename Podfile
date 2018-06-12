7773438edc89ed0a148daf0697d539fe348892ed
kongzichixiangjiao <58895347@qq.com>
2018-06-12 16:24:19 +0800

    Initial commit

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '9.0' 
+target '24Hours' do 
+ pod 'AMapSearch'
+ pod 'AMapLocation'
+end
---
+++ Pods/Target Support Files/Pods-24Hours/Pods-24Hours-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_24Hours : NSObject
+@end
+@implementation PodsDummy_Pods_24Hours
+@end
